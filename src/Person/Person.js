8f66fd39224f6e16f98bd1738a5cb6c4da7bd183
Faezeh <FVakili@users.noreply.github.com>
2018-02-13 17:23:15 +1100

    Revert "fvakili"
    This reverts commit 2755495d0a89fc5c6cc4fa30eff814795451dde1.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import './Person.css';
 
-const person = (props)=> {
-    // return <p> I'm Person and I am {Math.floor(Math.random() * 30)} years old! </p>
-    return (
-        <div className="Person" >
-            <p onClick={props.click} > I'm {props.name} and I am {props.age} years old! </p>
-            <p> {props.children}</p>
-            <input type="text" onChange={props.changed} value= {props.name} />
-        </div>
-    )
-
+const person = ()=> {
+    return <p> I'm a Person! </p>
 }
 
 export default person;
